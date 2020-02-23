11ad1e966c365ae0a6acdbc6f0797b1dd9bde749
Anastasia Mousa <49337566+anastasiamousa@users.noreply.github.com>
2020-02-23 21:39:24 +0200

    Update task No.1.py

--- task No.1.py
+++ task No.1.py
@@ -1,4 +1,4 @@
-- Taking the information stated at a table about the 7th semester courses at iee ihu department - 
+# - Taking the information stated at a table about the 7th semester courses at iee ihu department - 
 
 #Importing BeautifulSoup library
 Import requests
@@ -10,4 +10,24 @@ header = {
 }
 
 #Request 
+page = requests.get('https://wwww.iee.ihu.gr/en/udg_courses/', headers=header)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#reach 7th semester table
+tables = soup.find_all('table')
+table = tables[6]
+trs = table.find_all('tr')
+
+#creating a list for contents
+_7thInfo = list()
+contents = ['code','course','category','theory-h','lab-h','ects']
+
+#Loops through the table
+for tr in trs:
+  tds = tr.find_all('td')
+  data = dict()
+  
+  for num,td in enumerate(tds): 
+    
 
